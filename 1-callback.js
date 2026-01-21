72075ccc008fcf27a41be8a3b06ff2daef9fc630
Володимир Крісанов <volodimirkrisanov@gmail.com>
2026-01-21 10:58:22 +0100

    Update 1-callback.js

--- 1-callback.js
+++ 1-callback.js
@@ -1,1 +1,20 @@
+'use strict';
 
+const iterate = (object, callback) => {
+  for (const key in object) {
+    if (Object.prototype.hasOwnProperty.call(object, key)) {
+      callback(key, object[key], object);
+    }
+  }
+};
+
+// Тестування
+const obj = { a: 1, b: 2, c: 3 };
+iterate(obj, (key, value) => {
+  console.log({ key, value });
+});
+
+// Output:
+// { key: 'a', value: 1 }
+// { key: 'b', value: 2 }
+// { key: 'c', value: 3 }
